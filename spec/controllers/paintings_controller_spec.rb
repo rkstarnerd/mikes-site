f74772a6ea846b25225664cf2e49d6122ef506d0
rkstarnerd <gemille.ford@gmail.com>
2015-07-22 22:12:44 -0400

    test that painting controller show action sets the @painting variable

--- spec/controllers/paintings_controller_spec.rb
+++ spec/controllers/paintings_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe PaintingsController do
+  describe "GET show" do
+    it "sets the @painting variable" do
+      painting = Fabricate(:painting)
+      get :show, id: painting.id
+      expect(assigns(:painting)).to eq(painting)
+    end
+  end
 end
