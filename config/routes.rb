7c3b0128362b888f730a1550374ec0b4004f67d1
rkstarnerd <gemille.ford@gmail.com>
2015-07-23 21:14:44 -0400

    create routes to artist statements, reviews, cv, contacts, and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 MikesSite::Application.routes.draw do
   root 'paintings#index'
+  get '/statements', to: "artist#statements"
+  get '/reviews',    to: "artist#reviews"
+  get '/cv',         to: "artist#cv"
+  get '/contact',    to: "artist#contact"
+  get '/links',      to: "artist#links"
+
   resources :categories, only: [:show]
   resources :paintings, only: [:show]
 end
