e534c8471a984eb3d4cd696130e73ea318530ccd
rkstarnerd <gemille.ford@gmail.com>
2015-07-27 02:55:20 -0400

    shuffle paintings and assign shuffled paintings to carousel

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -3,6 +3,10 @@ class PaintingsController < ApplicationController
   
   def index
     @paintings = Painting.all
+    shuffled_paintings = @paintings.shuffle
+    @carousel_painting_one   = shuffled_paintings[0]
+    @carousel_painting_two   = shuffled_paintings[1]
+    @carousel_painting_three = shuffled_paintings[2]
   end
 
   def show
