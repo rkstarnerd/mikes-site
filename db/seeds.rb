eeb5bca4e7c8cc161e0d6c45a21d9a8521a15f88
rkstarnerd <gemille.ford@gmail.com>
2015-07-27 04:35:17 -0400

    reset db

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,7 @@ category_list.each do |id, name|
   Category.create(id: id, name: name)
 end
 
+#Paintings for Category 1
 Painting.create(artist: 'Michael Dixon', title: 'Bloomington 3', time_frame: '1997', medium: 'oil', surface: 'canvas', small_img_url: "/thumbnails/tn_Bloomington3.jpg", large_img_url: "/paintings/Bloomington3.jpg", height: 80, width: 60, category_id: 1)
 Painting.create(artist: 'Michael Dixon', title: 'Bloomington 2', time_frame: '1997', medium: 'oil', surface: 'canvas', small_img_url: "/thumbnails/tn_Bloomington2.jpg", large_img_url: "/paintings/Bloomington2.jpg", height: 64, width: 50, category_id: 1)
 Painting.create(artist: 'Michael Dixon', title: 'Last Graham Plaza', time_frame: '1997', medium: 'oil', surface: 'canvas', small_img_url: "/thumbnails/tn_LastGrahamPlaza.jpg", large_img_url: "/paintings/LastGrahamPlaza.jpg", height: 30, width: 40, category_id: 1)
@@ -27,3 +28,21 @@ Painting.create(artist: 'Michael Dixon', title: 'Banana Still Life', time_frame:
 Painting.create(artist: 'Michael Dixon', title: 'Yellow Still Life', time_frame: '1996', medium: 'oil', surface: 'canvas', small_img_url: "/thumbnails/tn_YellowStillLife.jpg", large_img_url: "/paintings/YellowStillLife.jpg", height: 28, width: 18, category_id: 1)
 Painting.create(artist: 'Michael Dixon', title: 'The Closet', time_frame: '1996', medium: 'oil', surface: 'canvas', small_img_url: "/thumbnails/tn_Closet.jpg", large_img_url: "/paintings/Closet.jpg", height: 40, width: 32, category_id: 1)
 Painting.create(artist: 'Michael Dixon', title: 'Margrave Bathroom', time_frame: '1996', medium: 'oil', surface: 'panel', small_img_url: "/thumbnails/tn_Bathroom.jpg", large_img_url: "/paintings/MargraveBathroom.jpg", height: 58, width: 38, category_id: 1)
+
+#Paintings for Category 2
+Painting.create(artist: 'Michael Dixon', title: 'Apple Tree 1', time_frame: '2000', medium: 'oil', surface: 'canvas', small_img_url: "/thumbnails/tn_AppleTree1.jpg", large_img_url: "/paintings/AppleTree1.jpg", height: 36, width: 34, category_id: 2)
+
+#Paintings for Category 3
+Painting.create(artist: 'Michael Dixon', title: 'The Cabinet Shop', time_frame: '2001', medium: 'oil', surface: 'canvas', small_img_url: "/thumbnails/tn_CabinetShop.jpg", large_img_url: "/paintings/CabinetShop.jpg", height: 45, width: 51, category_id: 3)
+
+#Paintings for Category 4
+Painting.create(artist: 'Michael Dixon', title: 'Lake Lanier 1: Wavy Cloud', time_frame: '2004', medium: 'gouache', surface: 'paper', small_img_url: "/thumbnails/tn_Lanier1.jpg", large_img_url: "/paintings/Lanier1.jpg", height: 9, width: 12, category_id: 4)
+
+#Paintings for Category 5
+Painting.create(artist: 'Michael Dixon', title: 'Still Life with Bit Brace', time_frame: '2008', medium: 'oil', surface: 'panel', small_img_url: "/thumbnails/tn_BitBrace.jpg", large_img_url: "/paintings/BitBrace w.jpg", height: 23, width: 32, category_id: 5)
+
+#Paintings for Category 6
+Painting.create(artist: 'Michael Dixon', title: 'Fairlie-Poplar', time_frame: '2009', medium: 'oil', surface: 'panel', small_img_url: "/thumbnails/tn_ViewFromtheGrantBldg.jpg", large_img_url: "/paintings/FairliePoplar.jpg", height: 48, width: 60, category_id: 6)
+
+#Paintings for Category 7
+Painting.create(artist: 'Michael Dixon', title: 'Athens 1', time_frame: '2009', medium: 'oil', surface: 'panel', small_img_url: "/thumbnails/tn_Athens1.jpg", large_img_url: "/paintings/Athens1.jpg", height: 24, width: 32, category_id: 7)
