68a07d43c4e4dc2d9440cc5fea894a2fb84ea90d
rkstarnerd <gemille.ford@gmail.com>
2015-07-27 03:41:32 -0400

    assign @categories

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -7,6 +7,8 @@ class PaintingsController < ApplicationController
     @carousel_painting_one   = shuffled_paintings[0]
     @carousel_painting_two   = shuffled_paintings[1]
     @carousel_painting_three = shuffled_paintings[2]
+
+    @categories = Category.all
   end
 
   def show
