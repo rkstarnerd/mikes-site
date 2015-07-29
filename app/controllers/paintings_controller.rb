59d8c8a63326319f2a4ec5235bec81ac1304b28f
rkstarnerd <gemille.ford@gmail.com>
2015-07-29 10:54:37 -0400

    make related_painting a private method

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -13,11 +13,16 @@ class PaintingsController < ApplicationController
 
   def show
     @painting = Painting.find(params[:id])
-    
-    other_paintings_in_category = @painting.category.paintings.map do |painting|
-      painting if painting != @painting
-    end
+    related_paintings(@painting)
+  end
 
+  private
+
+  def related_paintings(painting)
+    other_paintings_in_category = painting.category.paintings.map do |one_painting|
+      one_painting if one_painting != painting
+    end 
+    
     other_paintings_in_category.compact!
     
     if other_paintings_in_category.count > 4
