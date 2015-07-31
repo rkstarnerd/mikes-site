dff8ce691d40382253e453aac02002251dc0c54f
rkstarnerd <gemille.ford@gmail.com>
2015-07-31 15:57:53 -0400

    define subcategory variables in statements action

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,5 +1,9 @@
 class ArtistController < ApplicationController
-  def statements    
+  def statements
+    @rooftops = Category.find(6)
+    @factories = factories_subcategory.compact!
+    @cabinet_shop = Painting.find(21)
+    @apple_trees = apple_trees_subcategory.compact!
   end
 
   def reviews    
@@ -12,5 +16,18 @@ class ArtistController < ApplicationController
   end
 
   def links    
+  end
+
+  private
+
+  def factories_subcategory
+    Category.find(5).paintings.map {|p| p if p.title.include? "Factory"}
+  end
+
+  def people_subcategory    
+  end
+
+  def apple_trees_subcategory
+    Category.find(2).paintings.map {|painting| if painting.title.include? "Apple"}
   end
 end
