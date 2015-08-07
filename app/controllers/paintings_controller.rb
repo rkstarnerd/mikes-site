813b03a4450577374fd05ae1876c029982271002
rkstarnerd <gemille.ford@gmail.com>
2015-08-06 21:33:14 -0400

    shuffle paintings in index

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -2,7 +2,7 @@
 class PaintingsController < ApplicationController
   
   def index
-    shuffled_paintings = Painting.all
+    shuffled_paintings = Painting.all.shuffle
     @carousel_painting_one   = shuffled_paintings[0]
     @carousel_painting_two   = shuffled_paintings[1]
     @carousel_painting_three = shuffled_paintings[2]
