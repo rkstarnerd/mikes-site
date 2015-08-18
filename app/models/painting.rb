c68cd32f0d6cce98bff4abe41c20ed02ea4dfd1f
rkstarnerd <gemille.ford@gmail.com>
2015-08-18 12:51:55 -0400

    remove time_frame validation

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,4 +1,4 @@
 class Painting < ActiveRecord::Base
   belongs_to :category
-  validates_presence_of :artist, :title, :time_frame, :medium, :surface
+  validates_presence_of :artist, :title, :medium, :surface
 end
