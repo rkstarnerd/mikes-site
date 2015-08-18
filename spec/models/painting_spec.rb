65e7df19e7265c6840f176054af5c9afd45c7f6e
rkstarnerd <gemille.ford@gmail.com>
2015-08-18 12:52:21 -0400

    remove time_frame validation test

--- spec/models/painting_spec.rb
+++ spec/models/painting_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Painting do
   it { should validate_presence_of(:artist) }
   it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:time_frame) }
   it { should validate_presence_of(:medium) }
   it { should validate_presence_of(:surface) }
   it { should belong_to           (:category) }
