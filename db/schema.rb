d7381a6b9a0fa0289ebd039ced84dea6a594016d
rkstarnerd <gemille.ford@gmail.com>
2015-09-21 10:04:41 -0400

    add artist_id column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921134101) do
+ActiveRecord::Schema.define(version: 20150921140353) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "username"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150921134101) do
     t.datetime "updated_at"
     t.integer  "category_id"
     t.string   "magnified_img_url"
+    t.integer  "artist_id"
   end
 
 end
