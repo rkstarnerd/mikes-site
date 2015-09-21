b1fd9e0ff602eb3d6b78aefae4e310724e5b3003
rkstarnerd <gemille.ford@gmail.com>
2015-09-21 09:50:50 -0400

    remove time_frame from fabrication; it is no longer used

--- spec/fabricators/painting_fabricator.rb
+++ spec/fabricators/painting_fabricator.rb
@@ -1,7 +1,6 @@
 Fabricator(:painting) do
   artist  { Faker::Name.name }
   title   { Faker::Lorem.words(3).join(" ")}
-  time_frame { Faker::Number.number(4) }
   medium  { Faker::Lorem.word }
   surface { Faker::Lorem.word }
 end
