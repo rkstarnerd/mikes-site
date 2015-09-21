d986d4dd25a39909011eb3271d90eb61e85c74e4
rkstarnerd <gemille.ford@gmail.com>
2015-09-21 09:46:14 -0400

    create artist table, model, spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825170633) do
+ActiveRecord::Schema.define(version: 20150921134101) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
