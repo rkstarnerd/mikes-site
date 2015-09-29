778b91f588f7f61658a5262c087a3412d7e7c044
rkstarnerd <gemille.ford@gmail.com>
2015-09-28 21:33:32 -0400

    include SluggableGemille and specify sluggable_column

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
+  include SluggableGemille
+
   has_many :paintings
+
+  sluggable_column :name
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,5 +1,9 @@
 class Painting < ActiveRecord::Base
+  include SluggableGemille
+
   belongs_to :category
   belongs_to :artist
   validates_presence_of :title, :medium, :surface
+
+  sluggable_column :title
 end
