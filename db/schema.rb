ab9ccfcb7ca05113cd4f3850aec86ddcecff60dc
rkstarnerd <gemille.ford@gmail.com>
2015-09-28 21:39:01 -0400

    add slug column to Category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921143148) do
+ActiveRecord::Schema.define(version: 20150929013715) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "username"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150921143148) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
   create_table "paintings", force: :cascade do |t|
